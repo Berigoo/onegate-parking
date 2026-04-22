0488d54d0ae4aa6cd948d1a476f2e850a992520a
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-22 08:20:27 +0700

    t7

--- bsp/__init__.py
+++ bsp/__init__.py
@@ -1,4 +1,8 @@
+import os
 from .interface import BSP
+
+OPI_ZERO = os.getenv('OPI_ZERO', 'false').lower() in ('true', '1', 'yes')
+RASPI = os.getenv('RASPI', 'false').lower() in ('true', '1', 'yes')
 
 if OPI_ZERO:
     from bsp.opizero import BSPOpiZero as _impl
