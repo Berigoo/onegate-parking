185e5ec4ffd6124873f9954c8efa0607cd3da810
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-22 21:38:19 +0700

    test

---
+++ test/app/tasks/test_GateController.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import Mock, MagicMock, patch, call
+from app.tasks import GateController
+from app.core import SessionQueue
+from app.domain import StateEvent, EventType
+import time
+from bsp import bsp
+
+class TestGateControllerHWTests:
+    def test_when_open():        # pls pay attentiona to relay 'click' sound
+        bsp.bsp_init()
+        gate = GateController()
+        gate.open()
+        time.sleep(1)
+
+    def test_when_close():
+        bsp.bsp_init()
+        gate = GateController()
+        gate.close()
+        time.sleep(1)
+
+    def test_when_close():
+        bsp.bsp_init()
+        gate = GateController()
+        gate.open()
+        gate.hold()
+        time.sleep(1)
