6829f678aadad3c821cafb9b52241d4527f9e9ef
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-22 21:39:42 +0700

    test

--- test/app/tasks/test_GateController.py
+++ test/app/tasks/test_GateController.py
@@ -7,19 +7,19 @@ import time
 from bsp import bsp
 
 class TestGateControllerHWTests:
-    def test_when_open():        # pls pay attentiona to relay 'click' sound
+    def test_when_open(self):        # pls pay attentiona to relay 'click' sound
         bsp.bsp_init()
         gate = GateController()
         gate.open()
         time.sleep(1)
 
-    def test_when_close():
+    def test_when_close(self):
         bsp.bsp_init()
         gate = GateController()
         gate.close()
         time.sleep(1)
 
-    def test_when_close():
+    def test_when_close(self):
         bsp.bsp_init()
         gate = GateController()
         gate.open()
