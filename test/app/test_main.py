592e9428aeeebead022c97c75ffceba1ce51b382
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-23 02:26:47 +0700

    test

--- test/app/test_main.py
+++ test/app/test_main.py
@@ -908,6 +908,57 @@ class TestMainHWTests:
 
         assert isinstance(ctx._state, Idle)
 
+    def test_goldinng_the_gate(self):
+        bsp.bsp_init()
+        events_queue = SessionQueue()
+        session_queue = SessionQueue()
+        intercom = IntercomRelayMonitor(events_queue)
+        vld = VLDMonitor(events_queue)
+        gate_ctrl = GateController()
+        timer_mgr = TimerManager(events_queue)
+        dm = DisplayManager()
+        ctx = SystemStateContext("Idle", vld, None, None, intercom, None, gate_ctrl, timer_mgr, session_queue, dm)
+        intercom.start()
+        vld.start()
+
+
+        event = StateEvent(
+            type=EventType.CARD_TAP,
+            payload=None
+        )
+        events_queue.put(event)
+        event = StateEvent(
+            type=EventType.CARD_OUT_VALID,
+            payload={
+                        "uid": "11223344",
+                        "number": "1231232",
+                        "is_valid": True
+                    }
+        )
+        events_queue.put(event)
+
+        ev = events_queue.get()
+        ctx.do(ev)
+        ev = events_queue.get()
+        ctx.do(ev)
+        
+        print('waiting for vehicle gone...')
+
+        ev = events_queue.get()
+        ctx.do(ev)
+        ev = events_queue.get()
+        ctx.do(ev)
+
+        print('waiting another vehicle step on VLD...')
+        
+        ev = events_queue.get()
+        ctx.do(ev)
+        
+        intercom.stop()
+        vld.stop()
+
+        assert isinstance(ctx._state, HoldingGate)
+        
 
         
         
