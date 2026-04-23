37a37121076349d277915b2a3b6fa0f3e154f5da
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-23 16:53:03 +0700

    test

--- test/app/tasks/test_CardInValidatorTask.py
+++ test/app/tasks/test_CardInValidatorTask.py
@@ -5,7 +5,7 @@ import time
 from bsp import bsp
 from unittest.mock import Mock, MagicMock, patch, call
 from app.tasks import IntercomRelayMonitor, GateController, TimerManager, VLDMonitor, CardValidatorIn
-from app.core import SessionQueue, Logger
+from app.core import SessionQueue, Logger, DisplayManager
 from app.domain import StateEvent, EventType
 
 
