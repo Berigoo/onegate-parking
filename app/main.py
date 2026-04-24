09fa21c20dddabd561f5f0a041b3f35550812e6e
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-24 09:24:26 +0700

    test

--- app/main.py
+++ app/main.py
@@ -29,20 +29,20 @@ class Application:
         self.events_queue = SessionQueue()
         self.sessions_queue = SessionQueue() # may invalid
         self.vld_monitor = VLDMonitor(self.events_queue)
-        # self.card_validator_in = CardValidatorIn("/dev/ttyUSB0", "/etc/onegate-parking/cards.db", self.events_queue)
-        # self.card_validator_out = CardValidatorOut("/dev/ttyUSB1", "/etc/onegate-parking/cards.db", self.events_queue)
+        self.card_validator_in = CardValidatorIn("/dev/ttyUSB0", "/etc/onegate-parking/cards.db", self.events_queue)
+        self.card_validator_out = CardValidatorOut("/dev/ttyUSB1", "/etc/onegate-parking/cards.db", self.events_queue)
         self.intercom_relay = IntercomRelayMonitor(self.events_queue)
         self.gate_ctrl = GateController()
         self.timer_mgr = TimerManager(self.events_queue)
-        self.dm = DisplayManager()
         
         # Start monitor services
         self.vld_monitor.start()
-        # self.card_validator_in.start()
-        # self.card_validator_out.start()
+        self.card_validator_in.start()
+        self.card_validator_out.start()
         self.intercom_relay.start()
 
         # Camera workers and manager
+        self.dm = DisplayManager()
         self.dw = DisplayWorker()
         self.dw.start()
         self.camera = CameraMonitor()
@@ -52,11 +52,11 @@ class Application:
         self.dm.set_text(TextType.WELCOME)
 
         # Initialize state machine context
-        # self.ctx = SystemStateContext("Idle", self.vld_monitor, self.card_validator_in, self.card_validator_out, self.intercom_relay, self.camera, self.gate_ctrl, self.timer_mgr, self.sessions_queue, self.dm)
+        self.ctx = SystemStateContext("Idle", self.vld_monitor, self.card_validator_in, self.card_validator_out, self.intercom_relay, self.camera, self.gate_ctrl, self.timer_mgr, self.sessions_queue, self.dm)
         
     def __loop(self):
-        # ev = self.events_queue.get()
-        # self.ctx.do(ev)
+        ev = self.events_queue.get()
+        self.ctx.do(ev)
         True
 
     def start(self):
--- app/states/SerialDataProcessing.py
+++ app/states/SerialDataProcessing.py
@@ -12,7 +12,9 @@ class SerialDataProcessing(SystemState):
         self.context.timer_mgr.start(STATE_TIMEOUT, {"issuer": type(self).__name__})
         
     def execute(self):
+        self.__check_or_create()
         ev = self.context.current_event.type
+        
         match ev:
             case EventType.CARD_IN_VALID:
                 self.context.timer_mgr.stop()
@@ -34,15 +36,18 @@ class SerialDataProcessing(SystemState):
                     
                 else:
                     # TODO info message, and maybe add sleep, so it can be rendered for n sec
+                    self.context.timer_mgr.stop()
                     self.context.logger.warning("Card invalid")
                     self.context.set_state("Idle")
 
             case EventType.CARD_OUT_VALID:
                 self.context.timer_mgr.stop()
                 if self.context.current_event.payload["is_valid"]:
+                    self.context.timer_mgr.stop()
                     # do not care about "user already entered ?" info
                     self.context.set_state("AddingToQueue")
                 else:
+                    self.context.timer_mgr.stop()
                     # TODO info message, and maybe add sleep, so it can be rendered for n sec
                     self.context.logger.warning("Card invalid")
                     self.context.set_state("Idle")
@@ -52,5 +57,17 @@ class SerialDataProcessing(SystemState):
             case EventType.GENERIC_TIMEOUT:
                 self.context.set_state("Idle")
             
-        
-        
+        def __check_or_create(self):
+            conn = sqlite3.connect(ENTERED_USERS_DB)
+            cursor = conn.cursor()
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS entered_users (
+                timestamp TIMESTAMP,
+                uid TEXT,
+                )
+            """)
+
+            conn.commit()
+            conn.close()
+            
