8a8fdef7bf3857117ea1f28e1182d9c5dcf2725d
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-28 15:31:02 +0700

    refactor: to gpiozero

--- bsp/raspi/gpio.py
+++ bsp/raspi/gpio.py
@@ -1,17 +1,26 @@
 from . import config
-import RPi.GPIO as GPIO
+import gpiozero
+# import RPi.GPIO as GPIO
 import time
 
+_VLD = None
+_INTERCOM = None
+_GATE_HIGH = None
+_GATE_LOW = None
+
 def init_gpio():
-    GPIO.setmode(GPIO.BCM)
+    global _VLD, _INTERCOM, _GATE_HIGH, _GATE_LOW
+    # GPIO.setmode(GPIO.BCM)
     
-    # input devices
-    GPIO.setup(config.PIN_IN_VLD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(config.PIN_IN_INTERCOM_RELAY1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # # input devices
+    # GPIO.setup(config.PIN_IN_VLD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.setup(config.PIN_IN_INTERCOM_RELAY1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
-    # output device
-    GPIO.setup(config.PIN_BOOM_GATE_HIGH, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(config.PIN_BOOM_GATE_LOW, GPIO.OUT, initial=GPIO.HIGH)
+    # # output device
+    # GPIO.setup(config.PIN_BOOM_GATE_HIGH, GPIO.OUT, initial=GPIO.HIGH)
+    # GPIO.setup(config.PIN_BOOM_GATE_LOW, GPIO.OUT, initial=GPIO.HIGH)
+
+    _INTERCOM = gpiozero.DigitalInputDevice(config.PIN_IN_INTERCOM_RELAY1, pull_up=True, active_state=False, bounce_time=2)
 
 def deinit_gpio():
     GPIO.cleanup()
@@ -20,7 +29,9 @@ def read_vld_in():      # read in vld state
     return GPIO.input(config.PIN_IN_VLD)
     
 def read_intercom(): # read intercom relay1 state
-    return GPIO.input(config.PIN_IN_INTERCOM_RELAY1)
+    global _INTERCOM
+    return _INTERCOM.value
+    # return GPIO.input(config.PIN_IN_INTERCOM_RELAY1)
 
 def write_boom_gate(state):
     if state is True:
