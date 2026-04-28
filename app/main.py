98c5422267a134aef744cef6f2890a81d71ec63f
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-28 20:27:34 +0700

    rename

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ class Application:
         self.events_queue = SessionQueue()
         self.sessions_queue = SessionQueue() # may invalid
         self.vld_monitor = VLDMonitor(self.events_queue)
-        self.card_validator_in = CardValidatorIn("/dev/ttyUSB1", self.events_queue)
+        self.card_validator_in = CardValidatorIn("/dev/ttyUSB0", self.events_queue)
         self.card_validator_out = CardValidatorOut("/dev/card_reader_out", self.events_queue)
         self.intercom_relay = IntercomRelayMonitor(self.events_queue)
         self.gate_ctrl = GateController()
