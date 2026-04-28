e4bae1be7472cc18bf568a40fdd6b4885285f821
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-28 15:41:06 +0700

    refactor: to gpiozero

--- bsp/raspi/gpio.py
+++ bsp/raspi/gpio.py
@@ -20,13 +20,20 @@ def init_gpio():
     # GPIO.setup(config.PIN_BOOM_GATE_HIGH, GPIO.OUT, initial=GPIO.HIGH)
     # GPIO.setup(config.PIN_BOOM_GATE_LOW, GPIO.OUT, initial=GPIO.HIGH)
 
+    _VLD = gpiozero.DigitalInputDevice(config.PIN_IN_VLD, pull_up=True, active_state=False, bounce_time=2)
     _INTERCOM = gpiozero.DigitalInputDevice(config.PIN_IN_INTERCOM_RELAY1, pull_up=True, active_state=False, bounce_time=2)
 
+    _GATE_HIGH = gpiozero.DigitalOutputDevice(config.PIN_BOOM_GATE_HIGH, active_high=True, initial_value=False)
+    _GATE_LOW = gpiozero.DigitalOutputDevice(config.PIN_BOOM_GATE_LOW, active_high=True, initial_value=True)
+
 def deinit_gpio():
-    GPIO.cleanup()
+    # GPIO.cleanup()
+    pass
     
 def read_vld_in():      # read in vld state
-    return GPIO.input(config.PIN_IN_VLD)
+    global _VLD
+    return _VLD.value
+    # return GPIO.input(config.PIN_IN_VLD)
     
 def read_intercom(): # read intercom relay1 state
     global _INTERCOM
@@ -34,22 +41,36 @@ def read_intercom(): # read intercom relay1 state
     # return GPIO.input(config.PIN_IN_INTERCOM_RELAY1)
 
 def write_boom_gate(state):
+    global _GATE_HIGH, _GATE_LOW
     if state is True:
-        GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.HIGH)
-        GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.LOW)
+        _GATE_HIGH.on()
+        _GATE_LOW.off()
+        # GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.HIGH)
+        # GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.LOW)
     else:
-        GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.LOW)
-        GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.HIGH)
+        _GATE_HIGH.off()
+        _GATE_LOW.low()
+        # GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.LOW)
+        # GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.HIGH)
         
 def write_boom_gate_hold():
-    GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.HIGH)
-    GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.LOW)
+    global _GATE_HIGH, _GATE_LOW
+    _GATE_HIGH.off()
+    _GATE_LOW.low()
+    # GPIO.output(config.PIN_BOOM_GATE_HIGH, GPIO.HIGH)
+    # GPIO.output(config.PIN_BOOM_GATE_LOW, GPIO.LOW)
 
 def on_vld_in_high(cb): # execute 'cb' when in vld is HIGH
-    GPIO.add_event_detect(config.PIN_IN_VLD, GPIO.RISING, callback=cb, bouncetime=2000)
+    global _VLD
+    _VLD.when_activated = cb
+    # GPIO.add_event_detect(config.PIN_IN_VLD, GPIO.RISING, callback=cb, bouncetime=2000)
     
 def on_vld_in_low(cb):
-    GPIO.add_event_detect(config.PIN_IN_VLD, GPIO.FALLING, callback=cb, bouncetime=2000)
+    global _VLD
+    _VLD.when_deactivated = cb
+    # GPIO.add_event_detect(config.PIN_IN_VLD, GPIO.FALLING, callback=cb, bouncetime=2000)
 
 def on_intercom_relay_high(cb):
-    GPIO.add_event_detect(config.PIN_IN_INTERCOM_RELAY1, GPIO.RISING, callback=cb, bouncetime=2000)
+    global _INTERCOM
+    _INTERCOM.when_activated = cb
+    # GPIO.add_event_detect(config.PIN_IN_INTERCOM_RELAY1, GPIO.RISING, callback=cb, bouncetime=2000)
