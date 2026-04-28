f0118808a5955e1315ed5e8594b3c236eda7507d
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-28 22:41:21 +0700

    bypass

--- app/states/AddingToQueue.py
+++ app/states/AddingToQueue.py
@@ -4,6 +4,11 @@ from app.domain import EventType, TextType
 class AddingToQueue(SystemState):
     def init(self):
         self.context.sessions_queue.put(self.context.current_event) # guarantee CARD_IN_VALID or CARD_OUT_VALID or  INTERCOM_OVERRIDE. tmp: CARD_OUT_TAP
+        
+        # tmp
+        if self.context.current_event.type is EventType.CARD_OUT_TAP:
+            self.context.set_state("OpeningGate")
+        
         name = self.context.current_event.payload["name"]
         if name is None:
             self.context.dm.set_text("Selamat Datang")
