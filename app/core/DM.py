9adf5a6f9b38ae28b0d5320627f421969455ebfe
Berigoo <muhamadrizqifajri2@gmail.com>
2026-04-28 22:51:38 +0700

    fix

--- app/core/DM.py
+++ app/core/DM.py
@@ -29,9 +29,17 @@ class DisplayManager:           # A singleton class
         
         self.current_text: TextType = TextType.UNDEFINED
         self.current_color = (0, 255, 0)
-        
+
         cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow(WINDOW_NAME, np.zeros((100,100,3), dtype=np.uint8))
+
+        cv2.setWindowProperty(
+            WINDOW_NAME,
+            cv2.WND_PROP_FULLSCREEN,
+            cv2.WINDOW_FULLSCREEN
+        )
+        # cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+        # cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
         self._initialized = True
 
