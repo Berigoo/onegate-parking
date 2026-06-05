3eb8600e1fc2ad9ee1175fadba198bf6a11e5cb8
Berigoo <muhamadrizqifajri2@gmail.com>
2026-06-05 13:30:14 +0700

    feat(state): adding timer to holding gate state

--- app/states/HoldingGate.py
+++ app/states/HoldingGate.py
@@ -1,15 +1,19 @@
 from app.core import SystemState
 from app.domain import EventType
-from app.states import OpeningGate, HoldingGate
+
+STATE_TIMEOUT = 30
 
 class HoldingGate(SystemState):
     def init(self):
-        self.context.gate_ctrl.hold()        
-        
+        self.context.gate_ctrl.hold()
+        self.context.timer_mgr.start(STATE_TIMEOUT, {"issuer": type(self).__name__})
+
     def execute(self):
         ev = self.context.current_event.type
         match ev:
-            case EventType.VEHICLE_GONE:
+            case EventType.VEHICLE_GONE | EventType.GENERIC_TIMEOUT:
+                self.context.timer_mgr.stop()
                 self.context.set_state("ClosingGate")
             case EventType.INTERCOM_OVERRIDE:
+                self.context.timer_mgr.stop()
                 self.context.set_state("OpeningGate")
